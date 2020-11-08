791bd814abb1e45b6a11f44d0096c3c50b0643f3
EC2 Default User <ec2-user@ip-172-31-6-28.ec2.internal>
2020-11-08 00:11:06 +0000

    Create all location tables for database

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 from dotenv import load_dotenv
 import sqlalchemy
 import tables
+import requests
 from tables import BASE
 
 load_dotenv()
@@ -17,23 +18,49 @@ SESSION = SESSION_MAKER()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+CLIENT = discord.Client()
 
-@client.event
+COUNTRIES_API_URL = "https://restcountries.eu/rest/v2/"
+
+def add_country_and_borders(country):
+    country = SESSION.query(tables.Country).filter_by(ccode=country).first()
+    if not country:
+        resp = requests.get(COUNTRIES_API_URL + "alpha?codes=" + country)
+        print(resp.json()[0]["name"])
+        SESSION.add(tables.Country(country, resp.json()[0]["name"]))
+        SESSION.commit()
+
+@CLIENT.event
 async def on_ready():
-    print(f'{client.user} hasconnected to Discord!')
+    print(f'{CLIENT.user} hasconnected to Discord!')
 
-@client.event
+@CLIENT.event
 async def on_message(message):
     if message.author.bot:
         return
     passport = SESSION.query(tables.Traveler).filter_by(userId=str(message.author.id)).first()
     if not passport:
         print(message)
-        SESSION.add(tables.Traveler(str(message.author.id)))
-        SESSION.commit()
-        await message.channel.send("Hello " + message.author.nick + ", I hope you enjoy your trip on Air Discord!")
-        return
+        try:
+           
+            SESSION.add(tables.Traveler(str(message.author.id), "USA"))
+            SESSION.commit()
+            await message.channel.send("Hello " + message.author.nick + \
+                ", I hope you enjoy your trip on Air Discord! If you were just asking for something, please ask again as I may have missed it.  If you have further inquiries, please type !bot and I'll be right with you.")
+            return;
+        except Exception:
+            return
+    if(message.content[0] == '!'):
+        if(message.content.startswith("!bot")):
+            print("If you would like to")
+    passport.miles += 1
+    SESSION.commit()
+    if(passport.miles % 10 == 0):
+        await message.channel.send("YOU REACHED " + str(passport.miles) + " MILES!")
     await message.channel.send(message.content)
 
-client.run(TOKEN)
+resp = requests.get(COUNTRIES_API_URL + "alpha?codes=USA")
+print(resp)
+print(resp.json()[0]["name"])
+
+#CLIENT.run(TOKEN)
--- tables.py
+++ tables.py
@@ -1,16 +1,54 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 BASE = declarative_base()
 
 class Traveler(BASE):
     __tablename__ = "traveler"
-    userId = Column(String(50), primary_key=True)
+    uid = Column(String(50), primary_key=True)
     miles = Column(Integer)
+    location = Column(String(3), ForeignKey('country.ccode'))
     
-    def __init__(self, userId):
-        self.userId = userId
+    def __init__(self, userId, location):
+        self.uid = userId
         self.miles = 0
+        self.location = location
         
     def __repr__(self):
         return "USER %s HAS %i MILES" % (self.userId, self.miles)
+
+class Country(BASE):
+    __tablename__ = "country"
+    ccode = Column(String(3), primary_key=True)
+    name = Column(String(50))
+    
+    def __init__(self, ccode, name):
+        self.ccode = ccode
+        self.name = name
+        
+    def __repr__(self):
+        return "COUNTRY %s IS %s" % (self.ccode, self.name)
+
+class Border(BASE):
+    __tablename__ = "border"
+    ccodea = Column(String(3), ForeignKey('country.ccode'), primary_key=True)
+    ccodeb = Column(String(3), primary_key=True)
+    
+    def __init__(self, ccodea, ccodeb):
+        self.ccodea = ccodea
+        self.ccodeb = ccodeb
+        
+    def __repr__(self):
+        return "COUNTRY %s BORDERS %s" % (self.ccodea, self.ccodeb)
+
+class Stamp(BASE):
+    __tablename__ = "stamp"
+    uid = Column('uid', String(50), ForeignKey('traveler.uid'), primary_key=True)
+    code = Column('code', String(3), ForeignKey('country.ccode'), primary_key=True)
+    
+    def __init__(self, uid, ccode):
+        self.uid = uid
+        self.code = ccode
+        
+    def __repr__(self):
+        return "%s STAMPED AT %s" % (self.uid, self.code)
